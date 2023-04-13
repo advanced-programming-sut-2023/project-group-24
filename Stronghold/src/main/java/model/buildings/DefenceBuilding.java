e37bf0b4367c64179213fb6b3eae064b3cd524b1
arefzarezadeh <arefzarezade@gmail.com>
2023-04-13 21:34:02 +0330

    added staircase

--- Stronghold/src/main/java/model/buildings/DefenceBuilding.java
+++ Stronghold/src/main/java/model/buildings/DefenceBuilding.java
@@ -4,6 +4,7 @@ import model.LadderState;
 
 public class DefenceBuilding extends Building {
     private LadderState ladderState;
+    private LadderState staircase;
 
     public DefenceBuilding(Kingdom kingdom, BuildingType buildingType) {
         //TODO
@@ -13,7 +14,15 @@ public class DefenceBuilding extends Building {
         return ladderState;
     }
 
+    public LadderState getStaircase() {
+        return staircase;
+    }
+
     public void addLadder(LadderState ladderState) {
         //TODO set ladder state
+    }
+
+    public void addStaircase(LadderState staircase) {
+        //TODO set staircase
     }
 }
