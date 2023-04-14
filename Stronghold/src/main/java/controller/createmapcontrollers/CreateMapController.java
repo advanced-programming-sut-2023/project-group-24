86a1156365fea5e3e2afab917f5c885b3e879d53
arefzarezadeh <arefzarezade@gmail.com>
2023-04-14 19:07:55 +0330

    added the map maker

--- Stronghold/src/main/java/controller/createmapcontrollers/CreateMapController.java
+++
@@ -1,9 +0,0 @@
-package controller.createmapcontrollers;
-
-public class CreateMapController {
-    private Map map;
-
-    public CreateMapController(String id, int size) {
-        //TODO create new map with that id
-    }
-}
