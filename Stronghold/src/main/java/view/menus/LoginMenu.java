5fb795d68153566c1667f618fea48a1e53bf16da
pouria <pouriaghafouri1383@gmail.com>
2023-04-20 16:28:40 +0430

    login

--- Stronghold/src/main/java/view/menus/LoginMenu.java
+++ Stronghold/src/main/java/view/menus/LoginMenu.java
@@ -3,6 +3,8 @@ package view.menus;
 import controller.AppController;
 import controller.LoginMenuController;
 import utils.enums.MenusName;
+import view.enums.commands.Commands;
+import view.enums.messages.LoginMenuMessages;
 
 import java.util.regex.Matcher;
 
@@ -15,16 +17,43 @@ public class LoginMenu {
     }
 
     public void run() {
+        String command;
+        Matcher matcher;
         while (AppController.getCurrentMenu() == MenusName.LOGIN_MENU) {
-            //TODO get input from getCommands func and check type of input
+            command = GetInputFromUser.getUserInput();
+            if ((matcher = Commands.getMatcher(command, Commands.LOGIN_USER)) != null)
+                loginUser(matcher);
+            else if (Commands.getMatcher(command, Commands.FORGOT_PASSWORD) != null)
+                enterResetPasswordMenu();
+            else
+                System.out.println("Invalid command!");
         }
     }
 
     private void loginUser(Matcher matcher) {
-        //TODO connect it to controller and print errors and enter mainMenu
+        String username = matcher.group("username");
+        String password = matcher.group("password");
+        boolean stayLoggedIn = matcher.group("stayLoggedIn") != null;
+        LoginMenuMessages message = loginMenuController.loginUser(username, password, stayLoggedIn);
+        switch (message) {
+            case SUCCESS:
+                System.out.println("user logged in successfully!");
+                break;
+            case USER_NOT_FOUND:
+                System.out.println("User not found!");
+                break;
+            case INCORRECT_PASSWORD:
+                System.out.println("Incorrect password!\nYou have to wait for a while");
+                loginMenuController.makeDelayForIncorrectPassword();
+                break;
+            case INCORRECT_CAPTCHA:
+                System.out.println("You input incorrect captcha code!");
+            default:
+                break;
+        }
     }
 
     private void enterResetPasswordMenu() {
-        //TODO connect controller and set currentMenu resetPassMenu
+        AppController.setCurrentMenu(MenusName.RESET_PASSWORD_MENU);
     }
 }
